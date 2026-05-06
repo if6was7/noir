8d95dfb6e9e686aff851987817b52f9c41dbb79a
if6was7 <2wyowyo@gmail.com>
2026-05-06 23:19:01 +0300

    update pages

---
+++ noir/Pages/AdminFunction.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace noir.Pages
+{
+    public class AdminFunctionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
